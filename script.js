069273b0e7599845b56684bf0ed813d90dde454e
Nambala Sri Satya Harshita Reddy <harshitareddynss@gmail.com>
2026-04-03 23:21:19 +0530

    Added README file

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function () {
+    function reveal() {
+        let reveals = document.querySelectorAll(".reveal");
+
+        for (let i = 0; i < reveals.length; i++) {
+            let windowHeight = window.innerHeight;
+            let elementTop = reveals[i].getBoundingClientRect().top;
+            let elementVisible = 100;
+
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            }
+        }
+    }
+
+    window.addEventListener("scroll", reveal);
+    reveal(); // run once on load
+});
